accd20c62f5b2e7eb226f5e57c5f81098a0a2fb5
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-02-02 07:58:34 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -12,8 +12,35 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 @Client.on_message(filters.regex(pattern=".*http.*"))
-async def echo(bot, message):
-    await AddUser(bot, message)
+
+async def echo(bot, update):
+    if Config.LOG_CHANNEL:
+        try:
+            log_message = await update.forward(Config.LOG_CHANNEL)
+            log_info = "Message Sender Information\n"
+            log_info += "\nFirst Name: " + update.from_user.first_name
+            log_info += "\nUser ID: " + str(update.from_user.id)
+            log_info += "\nUsername: @" + update.from_user.username if update.from_user.username else ""
+            log_info += "\nUser Link: " + update.from_user.mention
+            await log_message.reply_text(
+                text=log_info,
+                disable_web_page_preview=True,
+                quote=True
+            )
+        except Exception as error:
+            print(error)
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await AddUser(bot, update)
+
+    if Config.UPDATES_CHANNEL:
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
+
+
+    logger.info(update.from_user)
+    url = update.text
     info_msg = await message.reply_text("<b>Processing... ⏳</b>", quote=True)
     youtube_dl_username = None
     youtube_dl_password = None
