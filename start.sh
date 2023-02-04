78dd6df1db3181ed317682ad408b95729ba49958
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-02-04 17:27:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/aceknox/UrlUploaderBot 
-cd /UrlUploaderBot
+git clone https://github.com/encryptedknox/t 
+cd /t
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait"
 python3 bot.py
