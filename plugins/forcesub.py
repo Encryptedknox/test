673f917ca7c51ebb499ec1f0e291ad192e1a8f9b
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-02-09 18:31:25 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -1,4 +1,4 @@
-# enable if you want it 
+# disable  if you don't want it 
 
 import asyncio
 from config import Config
@@ -17,7 +17,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry , You are Banned. Contact My [Support Group](https://t.me/knoxprojects).",
+                text="Sorry , You are Banned. Contact My [Support Group](https://t.me/knoxsupport).",
                 parse_mode="markdown",
                 disable_web_page_preview=True,
                 reply_to_message_id=message.message_id,
@@ -41,7 +41,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/knoxprojects).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/knoxsupport).",
             parse_mode="markdown",
             disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
