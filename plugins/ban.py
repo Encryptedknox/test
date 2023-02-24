29a2569e6a4c7945999706e381be0172e4fd9df2
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-02-24 11:02:47 +0530

    Update ban.py

--- plugins/ban.py
+++ plugins/ban.py
@@ -4,10 +4,8 @@ import os
 from database.database import Database
 from pyrogram import filters
 from pyrogram import Client
-from config import Config
-from config import DATABASE_URL
-from config import SESSION_NAME
-
+DATABASE_URL = mongodb+srv://Knoxop24:knoxop24@cluster0.f6m6fwo.mongodb.net/?retryWrites=true&w=majority
+SESSION_NAME = URLUPLOADERBOT
 db = Database(DATABASE_URL, SESSION_NAME)
 CURRENT_PROCESSES = {}
 CHAT_FLOOD = {}
