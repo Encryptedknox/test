856924c8582a1cb60e84ac316090db2fcaddeb56
Encryptedknox <100846845+Encryptedknox@users.noreply.github.com>
2023-02-25 08:39:11 +0530

    Update ban.py

--- plugins/ban.py
+++ plugins/ban.py
@@ -1,111 +1,90 @@
-import traceback
-import os
+from datetime import datetime, timedelta
 
-from database.database import Database
-from pyrogram import filters
-from pyrogram import Client
-DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://Knoxop24:knoxop24@cluster0.f6m6fwo.mongodb.net/?retryWrites=true&w=majority")
-SESSION_NAME = os.environ.get("SESSION_NAME","URLUPLOADERBOT")
-db = Database(DATABASE_URL, SESSION_NAME)
-CURRENT_PROCESSES = {}
-CHAT_FLOOD = {}
-broadcast_ids = {}
+from pyrogram import errors, filters
+from pyrogram.client import Client
+from pyrogram.types import Message
 
-async def sts(c, m):
-    
-    total_users = await db.total_users_count()
-    await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
+from common import bot_username, getFullName, owner_id
+
+timePeriods = {"s": "seconds", "m": "minutes",
+               "h": "hours", "d": "days", "w": "weeks"}
 
 
-async def ban(c, m):
-    
-    if len(m.command) == 1:
-        await m.reply_text(
-            f"Use this command to ban any user from the bot.\n\nUsage:\n\n`/ban_user user_id ban_duration ban_reason`\n\nEg: `/ban_user 1234567 28 You misused me.`\n This will ban user with id `1234567` for `28` days for the reason `You misused me`.",
-            quote=True
-        )
+@Client.on_message(filters.command(["kick", f"kick@{bot_username}"]))
+async def kick(client: Client, message: Message):
+    if (not (await message.chat.get_member("self")).privileges.can_restrict_members):
+        await message.reply_text("I need additional permissions to kick users")
         return
-    
+    match message.reply_to_message is not None, len(message.command) > 1:
+        case True, False:
+            effectiveId = message.reply_to_message.from_user.id
+        case False, True:
+            effectiveId = message.command[1]
+        case _:
+            await message.reply_text("Ambiguous user specified. Reply to the user or have their id as the argument")
+            return
+    if (not ((await message.chat.get_member(message.from_user.id)).privileges.can_restrict_members or message.from_user.id == owner_id)):
+        await message.reply_text("You must have user restricting permissions to use this command")
+        return
     try:
-        user_id = int(m.command[1])
-        ban_duration = int(m.command[2])
-        ban_reason = ' '.join(m.command[3:])
-        ban_log_text = f"Banning user {user_id} for {ban_duration} days for the reason {ban_reason}."
-        
-        try:
-            await c.send_message(
-                user_id,
-                f"You are banned to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ \n\n**Message from the admin**"
-            )
-            ban_log_text += '\n\nUser notified successfully!'
-        except:
-            traceback.print_exc()
-            ban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
-        await db.ban_user(user_id, ban_duration, ban_reason)
-        print(ban_log_text)
-        await m.reply_text(
-            ban_log_text,
-            quote=True
-        )
-    except:
-        traceback.print_exc()
-        await m.reply_text(
-            f"Error occoured! Traceback given below\n\n`{traceback.format_exc()}`",
-            quote=True
-        )
-
-
-async def unban(c, m):
-    if len(m.command) == 1:
-        await m.reply_text(
-            f"Use this command to unban any user.\n\nUsage:\n\n`/unban_user user_id`\n\nEg: `/unban_user 1234567`\n This will unban user with id `1234567`.",
-            quote=True
-        )
+        await message.chat.ban_member(effectiveId)
+    except errors.exceptions.bad_request_400.UserAdminInvalid:
+        await message.reply_text("This user is an admin")
         return
-    
+    await message.chat.unban_member(effectiveId)
+    await message.reply_text(f"Kicked {getFullName(await client.get_users(effectiveId))}")
+
+
+@Client.on_message(filters.command(["ban", f"ban@{bot_username}"]))
+async def ban(client: Client, message: Message):
+    if (not (await message.chat.get_member("self")).privileges .can_restrict_members):
+        await message.reply_text("I need additional permissions to ban users")
+        return
+    match message.reply_to_message is not None, len(message.command) == 1:
+        case True, False:
+            effectiveId = message.reply_to_message.from_user.id
+        case False, True:
+            effectiveId = message.command[1]
+        case _:
+            await message.reply_text("No user specified. Reply to the user or have their id as the argument")
+            return
+    if (not ((await message.chat.get_member(message.from_user.id)).privileges.can_restrict_members or message.from_user.id == owner_id)):
+        await message.reply_text("You must have user restricting permissions to use this command")
+        return
+    untilTime = datetime.now()
     try:
-        user_id = int(m.command[1])
-        unban_log_text = f"Unbanning user {user_id}"
-        
-        try:
-            await c.send_message(
-                user_id,
-                f"Your ban was lifted!"
-            )
-            unban_log_text += '\n\nUser notified successfully!'
-        except:
-            traceback.print_exc()
-            unban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
-        await db.remove_ban(user_id)
-        print(unban_log_text)
-        await m.reply_text(
-            unban_log_text,
-            quote=True
-        )
-    except:
-        traceback.print_exc()
-        await m.reply_text(
-            f"Error occoured! Traceback given below\n\n`{traceback.format_exc()}`",
-            quote=True
-        )
-
-
-async def _banned_usrs(c, m):
-    all_banned_users = await db.get_all_banned_users()
-    banned_usr_count = 0
-    text = ''
-    async for banned_user in all_banned_users:
-        user_id = banned_user['id']
-        ban_duration = banned_user['ban_status']['ban_duration']
-        banned_on = banned_user['ban_status']['banned_on']
-        ban_reason = banned_user['ban_status']['ban_reason']
-        banned_usr_count += 1
-        text += f"> **user_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, **Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
-    reply_text = f"Total banned user(s): `{banned_usr_count}`\n\n{text}"
-    if len(reply_text) > 4096:
-        with open('banned-users.txt', 'w') as f:
-            f.write(reply_text)
-        await m.reply_document('banned-users.txt', True)
-        os.remove('banned-users.txt')
+        durationStr = message.command[-1]
+        untilTime += timedelta(**
+                               {timePeriods[durationStr[-1]]: int(durationStr[:-1])})
+        msg = f"Banned {getFullName(await client.get_users(effectiveId))} for {durationStr}"
+    except (KeyError, ValueError):
+        await message.reply_text("Invalid time duration")
         return
-    await m.reply_text(reply_text, True)
+    except IndexError:
+        msg = f"Banned {getFullName(await client.get_users(effectiveId))} forever"
+    try:
+        await message.chat.ban_member(effectiveId, untilTime)
+    except errors.exceptions.bad_request_400.UserAdminInvalid:
+        await message.reply_text("This user is an admin")
+        return
+    await message.reply_text(msg)
+
+
+@Client.on_message(filters.command(["unban", f"unban@{bot_username}"]))
+async def unban(client: Client, message: Message):
+    if (not (await message.chat.get_member("self")).privileges.can_restrict_members):
+        await message.reply_text("I need additional permissions to unban users")
+        return
+    match message.reply_to_message is not None, len(message.command) == 1:
+        case True, False:
+            effectiveId = message.reply_to_message.from_user.id
+        case False, True:
+            effectiveId = message.command[1]
+        case _:
+            await message.reply_text("No user specified. Reply to the user or have their id as the argument")
+            return
+    if (not ((await message.chat.get_member(message.from_user.id)).privileges.can_restrict_members or message.from_user.id == owner_id)):
+        await message.reply_text("You must have user restricting permissions to use this command")
+        return
+    await message.chat.unban_member(effectiveId)
+    await message.reply_text(f"Unbanned {getFullName(await client.get_users(effectiveId))}")
